21f5f3a59b15d15336e3a0eaf3a56553ca82a584
Mindful Fragments <mindful.fragments@gmail.com>
2016-05-20 00:55:57 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Plotbunnysharing::Application.config.secret_key_base = '6fd972c80985ef0cdde0f85e9de08ef1eaa2ed132a4338d86262ec09ca745fa578c7f5a836e28e1e6fa17b63a2da1037c50f6dd21378a07177d85b1a97131e63'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Plotbunnysharing::Application.config.session_store :cookie_store, key: '_plotbunnysharing_session'
