cf772f5892b0ed4e38519d7eadc32036e20109de
Mindful Fragments <mindful.fragments@gmail.com>
2016-05-20 01:26:49 +0000

    Created home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Plotbunnysharing::Application.routes.draw do
+  root 'ideas#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
